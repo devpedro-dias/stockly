91638225365b562c92929cfc8054e373b3ff46c4
Pedro Dias <pedrocorchogdev@gmail.com>
2025-06-05 18:12:57 -0300

    feat: ensure sale product can be deleted

---
+++ app/sales/_components/table-dropdown-menu.tsx
@@ -0,0 +1,62 @@
+import { Button } from "@/app/_components/ui/button";
+import { DialogTrigger } from "@/app/_components/ui/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/app/_components/ui/dropdown-menu";
+import { Product } from "@prisma/client";
+import {
+  ClipboardCopyIcon,
+  EditIcon,
+  MoreHorizontalIcon,
+  TrashIcon,
+} from "lucide-react";
+
+interface SalesTableDropDownMenuProps {
+  product: Pick<Product, "id">;
+  onDelete: (productId: string) => void;
+}
+const SalesTableDropDownMenu = ({
+  product,
+  onDelete,
+}: SalesTableDropDownMenuProps) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost">
+          <MoreHorizontalIcon size={16} />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>Ações</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          className="gap-1.5"
+          onClick={() => navigator.clipboard.writeText(product.id)}
+        >
+          <ClipboardCopyIcon size={16} />
+          Copiar ID
+        </DropdownMenuItem>
+        <DialogTrigger asChild>
+          <DropdownMenuItem className="gap-1.5">
+            <EditIcon size={16} />
+            Editar
+          </DropdownMenuItem>
+        </DialogTrigger>
+        <DropdownMenuItem
+          className="gap-1.5"
+          onClick={() => onDelete(product.id)}
+        >
+          <TrashIcon size={16} />
+          Deletar
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default SalesTableDropDownMenu;
