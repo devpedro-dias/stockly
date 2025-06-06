c7b7a0630519233de23568c33a8e7eb6322f47ab
Pedro Dias <pedrocorchogdev@gmail.com>
2025-06-06 16:09:41 -0300

    feat: add product dto

--- app/_data-access/product/get-products.ts
+++ app/_data-access/product/get-products.ts
@@ -2,19 +2,14 @@ import "server-only";
 
 import { db } from "@/app/_lib/prisma";
 import { Product } from "@prisma/client";
-// import { unstable_cache } from "next/cache";
 
-export const getProducts = async (): Promise<Product[]> => {
-  return await db.product.findMany({});
+export interface ProductDto extends Product {
+  status: "IN_STOCK" | "OUT_OF_STOCK";
+}
+export const getProducts = async (): Promise<ProductDto[]> => {
+  const products = await db.product.findMany({});
+  return products.map((product) => ({
+    ...product,
+    status: product.stock > 0 ? "IN_STOCK" : "OUT_OF_STOCK",
+  }));
 };
-
-// Caching with unstable_cache
-// export const cachedGetProducts = unstable_cache(getProducts, ["get-products"], {
-//   revalidate: 5,
-// });
-
-// Caching with revalidateTag
-// export const cachedGetProducts = unstable_cache(getProducts, ["getProducts"], {
-//   tags: ["get-products"], is used to revalidate cache when revalidateTag is called passing the tag
-//   revalidate: 5,
-// });
