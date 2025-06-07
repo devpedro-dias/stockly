cff5527d8220d8bd7647b05f4b32f034088ca7c5
Pedro Dias <pedrocorchogdev@gmail.com>
2025-06-07 16:00:24 -0300

    feat: add streaming to all dashboard page

--- app/(dashboard)/_components/summary-card.tsx
+++ app/(dashboard)/_components/summary-card.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from "react";
 
-export const SummaryCardIcon = ({ children }: { children: ReactNode }) => {
+const SummaryCardIcon = ({ children }: { children: ReactNode }) => {
   return (
     <div className="mb-2 flex h-9 w-9 items-center justify-center rounded-md bg-emerald-500 bg-opacity-10 text-emerald-500">
       {children}
@@ -8,11 +8,11 @@ export const SummaryCardIcon = ({ children }: { children: ReactNode }) => {
   );
 };
 
-export const SummaryCardTitle = ({ children }: { children: ReactNode }) => {
+const SummaryCardTitle = ({ children }: { children: ReactNode }) => {
   return <p className="text-sm font-medium text-slate-500">{children}</p>;
 };
 
-export const SummaryCardValue = ({ children }: { children: ReactNode }) => {
+const SummaryCardValue = ({ children }: { children: ReactNode }) => {
   return <p className="text-2xl font-semibold text-slate-900">{children}</p>;
 };
 
@@ -20,4 +20,22 @@ const SummaryCard = ({ children }: { children: ReactNode }) => {
   return <div className="rounded-xl bg-white p-6">{children}</div>;
 };
 
-export default SummaryCard;
+const SummaryCardSkeleton = () => {
+  return (
+    <div className="rounded-xl bg-white p-6">
+      <div className="space-y-2">
+        <div className="h-9 w-9 rounded-md bg-gray-200" />
+        <div className="h-5 w-[86.26px] rounded-md bg-gray-200" />
+        <div className="h-8 w-48 rounded-md bg-gray-200" />
+      </div>
+    </div>
+  );
+};
+
+export {
+  SummaryCard,
+  SummaryCardIcon,
+  SummaryCardTitle,
+  SummaryCardValue,
+  SummaryCardSkeleton,
+};
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,34 +1,23 @@
-import {
-  CircleDollarSignIcon,
-  DollarSign,
-  Package,
-  ShoppingBagIcon,
-} from "lucide-react";
+import { Suspense } from "react";
 import Header, {
   HeaderLeft,
   HeaderSubtitle,
   HeaderTitle,
 } from "../_components/header";
-import SummaryCard, {
-  SummaryCardIcon,
-  SummaryCardTitle,
-  SummaryCardValue,
-} from "./_components/summary-card";
-import { getDashboard } from "../_data-access/dashboard/get-dashboard";
-import { formatCurrency } from "../_helpers/currency";
-import RevenueChart from "./_components/revenue-chart";
-import MostSoldProductItem from "./_components/most-sold-product-item";
+import { Skeleton } from "../_components/ui/skeleton";
+import Last14DaysRevenueCard from "./_components/last-14-days-revenue-card";
+import {
+  MostSoldProducts,
+  MostSoldProductsSkeleton,
+} from "./_components/most-sold-products";
+import TodayRevenueCard from "./_components/today-revenue-card";
+import TotalInStockCard from "./_components/total-in-stock-card";
+import TotalProductsCard from "./_components/total-products-card";
+import TotalRevenueCard from "./_components/total-revenue-card";
+import TotalSalesCard from "./_components/total-sales-card";
+import { SummaryCardSkeleton } from "./_components/summary-card";
 
 const Home = async () => {
-  const {
-    totalRevenue,
-    todayRevenue,
-    totalSales,
-    totalProducts,
-    totalStock,
-    totalLast14DaysRevenue,
-    mostSoldProducts,
-  } = await getDashboard();
   return (
     <div className="m-8 flex w-full flex-col space-y-8 rounded-lg">
       <Header>
@@ -38,65 +27,41 @@ const Home = async () => {
         </HeaderLeft>
       </Header>
       <div className="grid grid-cols-2 gap-6">
-        <SummaryCard>
-          <SummaryCardIcon>
-            <DollarSign />
-          </SummaryCardIcon>
-          <SummaryCardTitle>Receita Total</SummaryCardTitle>
-          <SummaryCardValue>{formatCurrency(totalRevenue)}</SummaryCardValue>
-        </SummaryCard>
-
-        <SummaryCard>
-          <SummaryCardIcon>
-            <DollarSign />
-          </SummaryCardIcon>
-          <SummaryCardTitle>Receita Hoje</SummaryCardTitle>
-          <SummaryCardValue>{formatCurrency(todayRevenue)}</SummaryCardValue>
-        </SummaryCard>
+        <Suspense fallback={<SummaryCardSkeleton />}>
+          <TotalRevenueCard />
+        </Suspense>
+        <Suspense fallback={<SummaryCardSkeleton />}>
+          <TodayRevenueCard />
+        </Suspense>
       </div>
-
       <div className="grid grid-cols-3 gap-6">
-        <SummaryCard>
-          <SummaryCardIcon>
-            <CircleDollarSignIcon />
-          </SummaryCardIcon>
-          <SummaryCardTitle>Vendas Totais</SummaryCardTitle>
-          <SummaryCardValue>{totalSales}</SummaryCardValue>
-        </SummaryCard>
-        <SummaryCard>
-          <SummaryCardIcon>
-            <Package />
-          </SummaryCardIcon>
-          <SummaryCardTitle>Total em estoque</SummaryCardTitle>
-          <SummaryCardValue>{totalStock}</SummaryCardValue>
-        </SummaryCard>
-        <SummaryCard>
-          <SummaryCardIcon>
-            <ShoppingBagIcon />
-          </SummaryCardIcon>
-          <SummaryCardTitle>Produtos</SummaryCardTitle>
-          <SummaryCardValue>{totalProducts}</SummaryCardValue>
-        </SummaryCard>
+        <Suspense fallback={<SummaryCardSkeleton />}>
+          <TotalSalesCard />
+        </Suspense>
+        <Suspense fallback={<SummaryCardSkeleton />}>
+          <TotalInStockCard />
+        </Suspense>
+        <Suspense fallback={<SummaryCardSkeleton />}>
+          <TotalProductsCard />
+        </Suspense>
       </div>
 
       <div className="grid min-h-0 grid-cols-[minmax(0,2.5fr),minmax(0,1fr)] gap-6">
-        <div className="flex h-full flex-col overflow-hidden rounded-xl bg-white p-6">
-          <p className="text-lg font-semibold text-slate-900">Receita</p>
-
-          <p className="text-sm text-slate-400">Últimos 14 dias</p>
-
-          <RevenueChart data={totalLast14DaysRevenue} />
-        </div>
-        <div className="flex h-full flex-col overflow-hidden rounded-xl bg-white">
-          <p className="p-6 text-lg font-semibold text-slate-900">
-            Produtos mais vendidos
-          </p>
-          <div className="space-y-7 overflow-y-auto px-6 pb-6">
-            {mostSoldProducts.map((product) => (
-              <MostSoldProductItem key={product.productId} product={product} />
-            ))}
-          </div>
-        </div>
+        <Suspense
+          fallback={
+            <Skeleton className="bg-white p-6">
+              <div className="space-y-2">
+                <div className="h-5 w-[86.26px] rounded-md bg-gray-200" />
+                <div className="h-4 w-48 rounded-md bg-gray-200" />
+              </div>
+            </Skeleton>
+          }
+        >
+          <Last14DaysRevenueCard />
+        </Suspense>
+        <Suspense fallback={<MostSoldProductsSkeleton />}>
+          <MostSoldProducts />
+        </Suspense>
       </div>
     </div>
   );
